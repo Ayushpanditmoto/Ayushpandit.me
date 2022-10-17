4b258d8ba31e88b3e3083c9295f9cd1a22538b48
Ayush Pandit <ayushpanditmoto@gmail.com>
2022-10-17 11:31:26 +0530

    added about me changes

--- src/Pages/Aboutme.js
+++ src/Pages/Aboutme.js
@@ -1,27 +1,114 @@
 import React from 'react'
 import Header from '../Components/Header'
+import styled from 'styled-components'
+import photo from '../photo.png'
 
 function Aboutme() {
   return (
     <>
       <Header />
-      <div className='flex relative justify-center items-center h-[300px] bg-slate-500 w-screen'>
-        <div className='z-20 h-48 w-48 bg-blue-600'></div>
-        <div className='z-0 absolute bottom-100 left-1/2 h-48 w-48 bg-green-400'></div>
-      </div>
-      <div className=' h-[600px] bg-slate-500 w-screen'>
-        <div className='z-0 h-48 w-48 bg-blue-600'></div>
-        <div className='z-20 absolute left-1/2 h-48 w-48 bg-green-400'></div>
-        <ul>
-          <li>hey</li>
-          <li>there</li>
-        </ul>
-        <ul>
-          <li>world</li>
-        </ul>
-      </div>
+      <AboutmeContainer>
+        <AboutmePhoto>
+          <img src={photo} alt='aboutme' />
+        </AboutmePhoto>
+        <AboutmeText>
+          <h1>ABOUT ME</h1>
+          <p>
+            I am <span>Ayush Kumar Pandit</span> .Currently, I am
+          </p>
+          <p> pursuing my Bachelor of Engineering in </p>
+          <p>
+            <span>Metallurgy and Material Engineering</span> from
+          </p>
+          <p>
+            <span>Jadavpur University</span>, Kolkata. Currently,
+          </p>
+          <p> I am a full Stack Web And Flutter Developer </p>
+        </AboutmeText>
+        <Contactme>
+          <h1>CONTACT ME</h1>
+          <p>
+            <a href='mailto:ayushpanditmoto@gmail.com'>
+              <span>Email:</span> ayushpanditmoto@gmail.com
+            </a>
+          </p>
+          <p>
+            <a href='https://www.linkedin.com/in/Ayushpanditmoto/'>
+              <span>LinkedIn:</span> @Ayushpanditmoto
+            </a>
+          </p>
+        </Contactme>
+      </AboutmeContainer>
     </>
   )
 }
 
 export default Aboutme
+
+const AboutmeContainer = styled.div`
+  font-family: 'Poppins', sans-serif;
+  background-color: #000000;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 30px;
+  h1 {
+    font-size: 2rem;
+    font-weight: 600;
+    margin-bottom: 20px;
+  }
+  p {
+    font-weight: 700;
+    font-size: 1.2rem;
+    line-height: 1.5;
+  }
+  p span {
+    color: #ffffff;
+    background-color: #ff0000;
+    padding: 0 5px;
+    border-radius: 5px;
+  }
+`
+
+const AboutmePhoto = styled.div`
+  margin: 20px;
+  img {
+    width: 200px;
+    object-fit: cover;
+    border-radius: 10%;
+  }
+`
+
+const AboutmeText = styled.div`
+  margin: 20px;
+  h1 {
+    color: #fff;
+    font-size: 2rem;
+  }
+  p {
+    color: #fff;
+    font-size: 1.2rem;
+    line-height: 1.5;
+  }
+`
+
+const Contactme = styled.div`
+  margin: 20px;
+  h1 {
+    color: #fff;
+    font-size: 2rem;
+  }
+  p {
+    color: #fff;
+    font-size: 1.2rem;
+    line-height: 1.5;
+  }
+  p span {
+    color: #ffffff;
+    background-color: #ff0000;
+    padding: 0 5px;
+    border-radius: 5px;
+  }
+`
